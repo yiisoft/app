6d7a4de50913c6c10a1b4db5ab4d1924b23eb569
Alexander Makarov <sam@rmcreative.ru>
2020-06-17 02:35:08 +0300

    Simplify layout

--- resources/layout/_footer.php
+++ resources/layout/_footer.php
@@ -1,22 +1,13 @@
-<?php
+<div class="columns is-mobile">
+    <div class="column has-text-left has-text-light">
+        Left
+    </div>
 
-declare(strict_types=1);
+    <div class="column has-text-centered has-text-light">
+        Center
+    </div>
 
-use Yiisoft\Html\Html;
-
-/** @var \App\ApplicationParameters $applicationParameters */
-?>
-<?= Html::beginTag('div', $applicationParameters->getHeroFooterColumnOptions()) ?>
-
-    <?= Html::beginTag('div', $applicationParameters->getHeroFooterColumnLeftOptions()) ?>
-        <?= $applicationParameters->getHeroFooterColumnLeft() ?>
-    <?= Html::endTag('div') ?>
-
-    <?= Html::beginTag('div', $applicationParameters->getHeroFooterColumnCenterOptions()) ?>
-        <?= $applicationParameters->getHeroFooterColumnCenter() ?>
-    <?= Html::endTag('div') ?>
-
-    <?= Html::beginTag('div', $applicationParameters->getHeroFooterColumnRightOptions()) ?>
-        <?= $applicationParameters->getHeroFooterColumnRight() ?>
-    <?= Html::endTag('div') ?>
-<?= Html::endTag('div') ?>
+    <div class="column has-text-right has-text-light">
+        Right
+    </div>
+</div>
--- resources/layout/_head.php
+++ resources/layout/_head.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 use Yiisoft\Html\Html;
 
 /**
- * @var Yiisoft\View\WebView $this
+ * @var \Yiisoft\View\WebView $this
  * @var \App\ApplicationParameters $applicationParameters
  * @var string $csrf
  */
 ?>
 <head>
-    <?= Html::tag('meta', '', ['charset' => $applicationParameters->getCharset()]) ?>
-    <?= Html::tag('meta', '', ['http-equiv' => 'X-UA-Compatible', 'content' => 'IE=edge']) ?>
-    <?= Html::tag('meta', '', ['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1']) ?>
-    <?= Html::tag('meta', '', ['name' => 'csrf', 'content' => $csrf]) ?>
+    <meta charset="<?= Html::encode($applicationParameters->getCharset()) ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf" content="<?= $csrf ?>">
     <?php if ($this->getTitle() !== null): ?>
-        <?= Html::tag('title', Html::encode($this->getTitle())) ?>
+        <title><?= Html::encode($this->getTitle()) ?></title>
     <?php endif ?>
     <?php $this->head() ?>
 </head>
