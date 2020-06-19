143fc05ee2eab7ef9c9d31370726f018a9d341ce
Alexander Makarov <sam@rmcreative.ru>
2020-06-19 19:00:05 +0300

    Separate web providers (#17)

--- config/providers-web.php
+++ config/providers-web.php
@@ -2,9 +2,30 @@
 
 declare(strict_types=1);
 
+/* @var array $params */
+
+use App\Provider\MiddlewareProvider;
+use App\Provider\Psr17Provider;
+use App\Provider\SessionProvider;
+use App\Provider\WebViewProvider;
 use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
 
 return [
+    'yiisoft/yii-web/psr17' => Psr17Provider::class,
+    'yiisoft/yii-web/middleware' => MiddlewareProvider::class,
+    'yiisoft/yii-web/session' => [
+        '__class' => SessionProvider::class,
+        '__construct()' => [
+            $params['yiisoft/yii-web']['session']['options'],
+            $params['yiisoft/yii-web']['session']['handler']
+        ],
+    ],
+    'yiisoft/view/webview' => [
+        '__class' => WebViewProvider::class,
+        '__construct()' => [
+            $params['yiisoft/view']['defaultParameters'],
+        ],
+    ],
     ReverseBlockMerge::class => new ReverseBlockMerge(),
 ];
 
