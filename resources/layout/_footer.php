226131304053b9c30576e37d0a394f34656c1e0b
terabytesoftw <terabytesoftw@gmail.com>
2020-06-20 08:41:11 -0400

    Enhacement home page.

--- resources/layout/_footer.php
+++ resources/layout/_footer.php
@@ -1,13 +1,34 @@
 <div class="columns is-mobile">
     <div class="column has-text-left has-text-light">
-        Left
+        <i class="fas fa-copyright fa-inverse"></i>
+        <a href="https://www.yiiframework.com/" target="_blank">
+            Yii Framework - 2020.
+        </a>
     </div>
 
     <div class="column has-text-centered has-text-light">
-        Center
     </div>
 
     <div class="column has-text-right has-text-light">
-        Right
+        <span class="icon">
+            <a href="https://github.com/yiisoft" target="_blank">
+                <i class="fab fa-github fa-inverse" aria-hidden="true"></i>
+            </a>
+        </span>
+        <span class="icon">
+            <a href="https://join.slack.com/t/yii/shared_invite/enQtMzQ4MDExMDcyNTk2LTc0NDQ2ZTZhNjkzZDgwYjE4YjZlNGQxZjFmZDBjZTU3NjViMDE4ZTMxNDRkZjVlNmM1ZTA1ODVmZGUwY2U3NDA" target="_blank">
+                <i class="fab fa-slack fa-inverse" aria-hidden="true"></i>
+            </a>
+        </span>
+        <span class="icon">
+            <a href="https://www.facebook.com/groups/yiitalk" target="_blank">
+                <i class="fab fa-facebook-f fa-inverse" aria-hidden="true"></i>
+            </a>
+        </span>
+        <span class="icon">
+            <a href="https://twitter.com/yiiframework" target="_blank">
+                <i class="fab fa-twitter fa-inverse" aria-hidden="true"></i>
+            </a>
+        </span>
     </div>
 </div>
