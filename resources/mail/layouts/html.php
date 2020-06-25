b74cb51183a7d8bc85d5e2d35c605eca799c9c6d
terabytesoftw <terabytesoftw@gmail.com>
2020-06-25 12:50:42 -0400

    Fix correction layout html.

--- resources/mail/layouts/html.php
+++ resources/mail/layouts/html.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /* @var $content string Mail contents as view render result */
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -15,7 +15,7 @@ declare(strict_types=1);
 <?= $content ?>
 
 <footer style="margin-top: 5em">
--- <br>
+-- <br />
 Mailed by Yii
 </footer>
 
