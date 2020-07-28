6e2c657889cd6691a600fe0aa959eec345017c2d
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2020-07-28 12:15:35 -0400

    Fix scrutinizer issues. (#41)

--- src/Provider/Psr17Provider.php
+++ src/Provider/Psr17Provider.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace App\Provider;
 
 use Nyholm\Psr7\Factory\Psr17Factory;
-use Yiisoft\Di\Container;
-use Yiisoft\Di\Support\ServiceProvider;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ServerRequestFactoryInterface;
@@ -17,6 +15,8 @@ use Yiisoft\DataResponse\DataResponseFactory;
 use Yiisoft\DataResponse\DataResponseFactoryInterface;
 use Yiisoft\DataResponse\DataResponseFormatterInterface;
 use Yiisoft\DataResponse\Formatter\HtmlDataResponseFormatter;
+use Yiisoft\Di\Container;
+use Yiisoft\Di\Support\ServiceProvider;
 
 final class Psr17Provider extends ServiceProvider
 {
