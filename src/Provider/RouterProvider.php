bc0a514ec81bf52f1a0d557da19461024fb2d20c
Alexander Makarov <sam@rmcreative.ru>
2020-09-06 15:51:06 +0300

    Adjust to router changes

--- src/Provider/RouterProvider.php
+++ src/Provider/RouterProvider.php
@@ -7,6 +7,8 @@ namespace App\Provider;
 use Psr\Container\ContainerInterface;
 use Yiisoft\Di\Container;
 use Yiisoft\Di\Support\ServiceProvider;
+use Yiisoft\Router\Dispatcher;
+use Yiisoft\Router\DispatcherInterface;
 use Yiisoft\Router\FastRoute\UrlGenerator;
 use Yiisoft\Router\FastRoute\UrlMatcher;
 use Yiisoft\Router\Group;
@@ -30,6 +32,8 @@ final class RouterProvider extends ServiceProvider
      */
     public function register(Container $container): void
     {
+        $container->set(DispatcherInterface::class, Dispatcher::class);
+
         $container->set(RouteCollectorInterface::class, Group::create());
 
         $container->set(UrlMatcherInterface::class, function (ContainerInterface $container) {
