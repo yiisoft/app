6506cc18af1a74f3a1fdba12315f705823d81438
mj4444 <mj4444@mail.ru>
2020-09-18 04:40:46 +0500

    Remove from config not used function for creaete ContainerInterface

--- config/common.php
+++ config/common.php
@@ -9,10 +9,6 @@ use Psr\Container\ContainerInterface;
 /* @var array $params */
 
 return [
-    ContainerInterface::class => static function (ContainerInterface $container) {
-        return $container;
-    },
-
     ContactMailer::class => static function (ContainerInterface $container) use ($params) {
         return (new ContactMailer(
             $container->get(Mailer::class),
