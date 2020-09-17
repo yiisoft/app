fa855bab9b9a35f74b60e3d3db43da75c6f09c9e
mj4444 <mj4444@mail.ru>
2020-09-18 04:22:58 +0500

    Remove dubbing Aliases config from App

--- config/common.php
+++ config/common.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use App\Contact\ContactMailer;
 use App\Service\Mailer;
 use Psr\Container\ContainerInterface;
-use Yiisoft\Aliases\Aliases;
 
 /* @var array $params */
 
@@ -13,11 +12,6 @@ return [
     ContainerInterface::class => static function (ContainerInterface $container) {
         return $container;
     },
-
-    Aliases::class => [
-        '__class' => Aliases::class,
-        '__construct()' => [$params['aliases']],
-    ],
 
     ContactMailer::class => static function (ContainerInterface $container) use ($params) {
         return (new ContactMailer(
