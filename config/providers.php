5c53a55f5591c237349e36e3294184730123859a
Dmitriy Derepko <xepozz@list.ru>
2020-10-05 22:07:26 +0300

    Removed file log configuration

--- config/providers.php
+++ config/providers.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 /* @var array $params */
 
 use App\Provider\CacheProvider;
-use App\Provider\FileRotatorProvider;
-use App\Provider\FileTargetProvider;
 use App\Provider\LoggerProvider;
 use App\Provider\RouterProvider;
 use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
@@ -21,22 +19,6 @@ return [
         '__class' => CacheProvider::class,
         '__construct()' => [
             $params['yiisoft/cache-file']['file-cache']['path'],
-        ],
-    ],
-    'yiisoft/log-target-file/filerotator' => [
-        '__class' => FileRotatorProvider::class,
-        '__construct()' => [
-            $params['yiisoft/log-target-file']['file-rotator']['maxfilesize'],
-            $params['yiisoft/log-target-file']['file-rotator']['maxfiles'],
-            $params['yiisoft/log-target-file']['file-rotator']['filemode'],
-            $params['yiisoft/log-target-file']['file-rotator']['rotatebycopy']
-        ],
-    ],
-    'yiisoft/log-target-file/filetarget' => [
-        '__class' => FileTargetProvider::class,
-        '__construct()' => [
-            $params['yiisoft/log-target-file']['file-target']['file'],
-            $params['yiisoft/log-target-file']['file-target']['levels']
         ],
     ],
     'yiisoft/log/logger' =>  LoggerProvider::class,
