73416dda8590044d49f5985e350bf48497e9523c
wiperawa <wiperawa@gmail.com>
2020-10-08 21:08:53 +0500

    Replace FileRotatorProvider by LoggerProvider.php (#66)

--- config/providers.php
+++ config/providers.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 /* @var array $params */
 
+use App\Provider\LoggerProvider;
 use App\Provider\RouterProvider;
 use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
 use Yiisoft\Composer\Config\Builder;
@@ -13,14 +14,8 @@ return [
         '__class' => RouterProvider::class,
         '__construct()' => [Builder::require('routes')],
     ],
-    'yiisoft/log-target-file/filerotator' => [
-        '__class' => FileRotatorProvider::class,
-        '__construct()' => [
-            $params['yiisoft/log-target-file']['file-rotator']['maxfilesize'],
-            $params['yiisoft/log-target-file']['file-rotator']['maxfiles'],
-            $params['yiisoft/log-target-file']['file-rotator']['filemode'],
-            $params['yiisoft/log-target-file']['file-rotator']['rotatebycopy']
-        ],
-    ],
+
+    'LoggerProvider' => LoggerProvider::class,
+
     ReverseBlockMerge::class => new ReverseBlockMerge()
 ];
