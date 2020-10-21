d422dd3007ba97f5e9e64f006b3619fcf3140187
yiiliveext <yiiliveext@gmail.com>
2020-10-22 00:39:30 +0300

    Fix router provider (#77)

--- src/Provider/RouterProvider.php
+++ src/Provider/RouterProvider.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 
 namespace App\Provider;
 
-use Psr\Container\ContainerInterface;
 use Yiisoft\Di\Container;
 use Yiisoft\Di\Support\ServiceProvider;
-use Yiisoft\Router\FastRoute\UrlGenerator;
-use Yiisoft\Router\FastRoute\UrlMatcher;
 use Yiisoft\Router\Group;
 use Yiisoft\Router\RouteCollection;
 use Yiisoft\Router\RouteCollectorInterface;
-use Yiisoft\Router\UrlGeneratorInterface;
-use Yiisoft\Router\UrlMatcherInterface;
+use Yiisoft\Router\RouteCollectionInterface;
 use Yiisoft\DataResponse\Middleware\FormatDataResponse;
 
 final class RouterProvider extends ServiceProvider
@@ -30,18 +26,12 @@ final class RouterProvider extends ServiceProvider
      */
     public function register(Container $container): void
     {
-        $container->set(RouteCollectorInterface::class, Group::create());
-
-        $container->set(UrlMatcherInterface::class, function (ContainerInterface $container) {
-            $collector = $container->get(RouteCollectorInterface::class);
+        $container->set(RouteCollectionInterface::class, function (RouteCollectorInterface $collector) {
             $collector->addGroup(
-                Group::create(null, $this->routes)
-                    ->addMiddleware(FormatDataResponse::class)
+                Group::create(null, $this->routes)->addMiddleware(FormatDataResponse::class)
             );
 
-            return new UrlMatcher(new RouteCollection($collector));
+            return new RouteCollection($collector);
         });
-
-        $container->set(UrlGeneratorInterface::class, UrlGenerator::class);
     }
 }
