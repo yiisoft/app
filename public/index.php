0909288b231c2d71cd777827c73ed9b6915880f3
Dmitriy Derepko <xepozz@list.ru>
2020-10-25 20:41:19 +0300

    Move out error handler from application

--- public/index.php
+++ public/index.php
@@ -1,36 +1,45 @@
 <?php
 
-declare(strict_types=1);
-
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Yiisoft\Composer\Config\Builder;
 use Yiisoft\Di\Container;
 use Yiisoft\Http\Method;
 use Yiisoft\Yii\Web\Application;
+use Yiisoft\Yii\Web\ErrorHandler\ErrorHandler;
+use Yiisoft\Yii\Web\ErrorHandler\HtmlRenderer;
+use Yiisoft\Yii\Web\ErrorHandler\ThrowableRendererInterface;
 use Yiisoft\Yii\Web\SapiEmitter;
 use Yiisoft\Yii\Web\ServerRequestFactory;
 
-$autoload = dirname(__DIR__) . '/vendor/autoload.php';
-$c3 = dirname(__DIR__) . '/c3.php';
-
-if (is_file($c3)) {
-    require_once $c3;
-}
-
-if (!is_file($autoload)) {
-    die('You need to set up the project dependencies using Composer');
-}
-
-require_once $autoload;
-
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 // Don't do it in production, assembling takes it's time
 Builder::rebuild();
 $startTime = microtime(true);
+
+/**
+ * Register temporary error handler to catch error while container is building.
+ */
+$errorHandler = new ErrorHandler(new NullLogger(), new HtmlRenderer());
+/**
+ * Production mode
+ * $errorHandler = $errorHandler->withoutExposedDetails();
+ */
+$errorHandler->register();
 
 $container = new Container(
     require Builder::path('web'),
     require Builder::path('providers-web')
 );
 
+/**
+ * Configure error handler with real container-configured dependencies
+ */
+$errorHandler->setLogger($container->get(LoggerInterface::class));
+$errorHandler->setRenderer($container->get(ThrowableRendererInterface::class));
+
+$container = $container->get(ContainerInterface::class);
 $application = $container->get(Application::class);
 
 $request = $container->get(ServerRequestFactory::class)->createFromGlobals();
