239f4ff6bd78efc467c733b92ffdd7cbb8da1209
Wilmer Arambula <terabytesoftw@gmail.com>
2020-10-27 13:35:10 -0300

    Fixed tests again.

--- tests/_support/public/index.php
+++ tests/_support/public/index.php
@@ -3,6 +3,7 @@
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
+use Yiisoft\Aliases\Aliases;
 use Yiisoft\Composer\Config\Builder;
 use Yiisoft\Di\Container;
 use Yiisoft\Http\Method;
@@ -13,7 +14,12 @@ use Yiisoft\Yii\Web\ErrorHandler\ThrowableRendererInterface;
 use Yiisoft\Yii\Web\SapiEmitter;
 use Yiisoft\Yii\Web\ServerRequestFactory;
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__, 3) . '/vendor/autoload.php';
+$c3 = dirname(__DIR__, 3) . '/c3.php';
+
+if (is_file($c3)) {
+    require_once $c3;
+}
 
 // Don't do it in production, assembling takes it's time
 Builder::rebuild();
@@ -33,6 +39,11 @@ $container = new Container(
     require Builder::path('web'),
     require Builder::path('providers-web')
 );
+
+$aliases = $container->get(Aliases::class);
+
+$aliases->set('@public', __DIR__);
+$aliases->set('@assets', '@public/assets');
 
 /**
  * Configure error handler with real container-configured dependencies
