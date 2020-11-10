4845f5d153a21b7e41d27878d65ab66638600e86
Wilmer Arambula <terabytesoftw@gmail.com>
2020-11-10 11:55:51 -0300

    Fix config console.

--- config/providers.php
+++ config/providers.php
@@ -4,5 +4,12 @@ declare(strict_types=1);
 
 /** @var array $params */
 
+use Yiisoft\Composer\Config\Builder;
+use Yiisoft\Yii\Event\EventDispatcherProvider;
+
 return [
+    'yiisoft/event-dispatcher/eventdispatcher' => [
+        '__class' => EventDispatcherProvider::class,
+        '__construct()' => [Builder::require('events-web')]
+    ]
 ];
