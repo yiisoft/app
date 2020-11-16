4c9c24c0adf9a6ef87c024e17e8303bb2631d356
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2020-11-16 09:51:04 -0300

    Fix config after removing providers from yii-event

--- config/providers-web.php
+++ config/providers-web.php
@@ -4,12 +4,4 @@ declare(strict_types=1);
 
 /** @var array $params */
 
-use Yiisoft\Composer\Config\Builder;
-use Yiisoft\Yii\Event\EventDispatcherProvider;
-
-return [
-    'yiisoft/event-dispatcher/eventdispatcher' => [
-        '__class' => EventDispatcherProvider::class,
-        '__construct()' => [Builder::require('events-web')]
-    ]
-];
+return [];
--- config/providers.php
+++ config/providers.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
+
 /** @var array $params */
 
 return [
+    ReverseBlockMerge::class => new ReverseBlockMerge()
 ];
