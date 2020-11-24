24a32a9e2118bcb750992157973f3950bae66b95
Alexander Makarov <sam@rmcreative.ru>
2020-11-24 19:50:21 +0300

    Adjust config

---
+++ config/web/application.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\Factory\Definitions\Reference;
+
+return [
+
+];
--- config/web/middleware-dispatcher.php
+++ config/web/middleware-dispatcher.php
@@ -2,19 +2,22 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
 use Yiisoft\Csrf\CsrfMiddleware;
 use Yiisoft\ErrorHandler\ErrorCatcher;
+use Yiisoft\Injector\Injector;
 use Yiisoft\Middleware\Dispatcher\MiddlewareDispatcher;
 use Yiisoft\Router\Middleware\Router;
 use Yiisoft\Session\SessionMiddleware;
 
-return [
-    MiddlewareDispatcher::class => static fn (ContainerInterface $container) => $container->get(MiddlewareDispatcher::class)
-         ->withMiddlewares([
-             Router::class,
-             SessionMiddleware::class,
-             CsrfMiddleware::class,
-             ErrorCatcher::class,
-         ]),
+return [    
+    'application-dispatcher' => static function (Injector $injector) {
+        $middlewareDispatcher = $injector->make(MiddlewareDispatcher::class);
+
+        return $middlewareDispatcher->withMiddlewares([
+            Router::class,
+            SessionMiddleware::class,
+            CsrfMiddleware::class,
+            ErrorCatcher::class,
+        ]);
+    }
 ];
