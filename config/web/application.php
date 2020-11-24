17c3b8dadd7023ac99b52990e0128c217d47c8a5
Alexander Makarov <sam@rmcreative.ru>
2020-11-24 20:50:57 +0300

    Adjust config

--- config/web/application.php
+++ config/web/application.php
@@ -2,13 +2,26 @@
 
 declare(strict_types=1);
 
-use Yiisoft\Factory\Definitions\Reference;
+use Yiisoft\Csrf\CsrfMiddleware;
+use Yiisoft\ErrorHandler\ErrorCatcher;
+use Yiisoft\Injector\Injector;
+use Yiisoft\Middleware\Dispatcher\MiddlewareDispatcher;
+use Yiisoft\Router\Middleware\Router;
+use Yiisoft\Session\SessionMiddleware;
 
 return [
     Yiisoft\Yii\Web\Application::class => [
-        'class' => Yiisoft\Yii\Web\Application::class,
         '__construct()' => [
-            'dispatcher' => Reference::to('application-dispatcher'),
+            'dispatcher' => static function (Injector $injector) {
+                return ($injector->make(MiddlewareDispatcher::class))
+                    ->withMiddlewares([
+                        Router::class,
+                        SessionMiddleware::class,
+                        CsrfMiddleware::class,
+                        ErrorCatcher::class,
+                    ]
+                );
+            }
         ],
     ],
 ];
