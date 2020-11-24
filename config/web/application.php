86c09eaed19389ae635996b15b3d0709b306a2a6
Alexander Makarov <sam@rmcreative.ru>
2020-11-24 19:51:58 +0300

    Add app config

--- config/web/application.php
+++ config/web/application.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Factory\Definitions\Reference;
 
 return [
+    Yiisoft\Yii\Web\Application::class => [
+        'class' => Yiisoft\Yii\Web\Application::class,
+        '__construct()' => [
+            'dispatcher' => Reference::to('application-dispatcher')
+        ],
+    ],
 ];
