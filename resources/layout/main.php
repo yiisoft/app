6edbbf47ecb991a1173418e6424b964c1904743a
Vitalii Shkolin <vitalii@shkolin.net.ua>
2021-01-12 12:45:59 +0200

    Merge layouts

--- resources/layout/main.php
+++ resources/layout/main.php
@@ -6,6 +6,8 @@ use App\Asset\AppAsset;
 use App\Asset\CdnFontAwesomeAsset;
 use Yiisoft\Html\Html;
 use Yiisoft\I18n\Locale;
+use Yiisoft\Yii\Bulma\Nav;
+use Yiisoft\Yii\Bulma\NavBar;
 
 /**
  * @var App\ApplicationParameters $applicationParameters
@@ -30,15 +32,35 @@ $this->setJsVar($assetManager->getJsVar());
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Html::encode($locale->language()) ?>">
-    <?= $this->render('_head', ['applicationParameters' => $applicationParameters]) ?>
-    <?php $this->beginBody() ?>
-        <body>
+    <head>
+        <meta charset="<?= Html::encode($applicationParameters->getCharset()) ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php if ($this->getTitle() !== null): ?>
+            <title><?= Html::encode($this->getTitle()) ?></title>
+        <?php endif ?>
+        <?php $this->head() ?>
+    </head>
+    <body>
+        <?php $this->beginBody() ?>
             <section class="hero is-fullheight is-light">
                 <div class="hero-head has-background-black">
-                    <?= $this->render(
-                        '_menu',
-                        ['applicationParameters' => $applicationParameters, 'urlMatcher' => $urlMatcher]
-                    ) ?>
+                    <?= NavBar::widget()
+                        ->brandLabel($applicationParameters->getName())
+                        ->brandImage('/images/yii-logo.jpg')
+                        ->options(['class' => 'is-black', 'data-sticky' => '', 'data-sticky-shadow' => ''])
+                        ->itemsOptions(['class' => 'navbar-end'])
+                        ->begin() ?>
+
+                    <?= Nav::widget()
+                        ->currentPath(
+                            $urlMatcher->getCurrentUri() !== null
+                                ? $urlMatcher->getCurrentUri()->getPath()
+                                : ''
+                        )
+                        ->items([]) ?>
+
+                    <?= NavBar::end() ?>
                 </div>
                 <div class="hero-body is-light">
                     <div class="container has-text-centered">
@@ -46,11 +68,48 @@ $this->setJsVar($assetManager->getJsVar());
                     </div>
                 </div>
                 <div class="hero-footer has-background-black">
-                    <?= $this->render('_footer', ['applicationParameters' => $applicationParameters]) ?>
+                    <div class="columns is-mobile">
+                        <div class="column has-text-left has-text-light">
+                            <i class="fas fa-copyright fa-inverse is-hidden-mobile"></i>
+                            <a class="is-hidden-mobile" href="https://www.yiiframework.com/" target="_blank" rel="noopener">
+                                <?= date('Y') ?>  <?= Html::encode($applicationParameters->getName()) ?>
+                            </a>
+                            <a class="is-hidden-desktop is-size-6" href="https://www.yiiframework.com/" target="_blank" rel="noopener">
+                                <?= Html::encode($applicationParameters->getName()) ?>
+                            </a>
+                        </div>
+                        <div class="column has-text-centered has-text-light is-hidden-mobile"></div>
+                        <div class="column has-text-right has-text-light">
+                            <span class="icon">
+                                <a href="https://github.com/yiisoft" target="_blank" rel="noopener">
+                                    <i class="fab fa-github fa-inverse" aria-hidden="true"></i>
+                                </a>
+                            </span>
+                            <span class="icon">
+                                <a href="https://join.slack.com/t/yii/shared_invite/enQtMzQ4MDExMDcyNTk2LTc0NDQ2ZTZhNjkzZDgwYjE4YjZlNGQxZjFmZDBjZTU3NjViMDE4ZTMxNDRkZjVlNmM1ZTA1ODVmZGUwY2U3NDA" target="_blank" rel="noopener">
+                                    <i class="fab fa-slack fa-inverse " aria-hidden="true"></i>
+                                </a>
+                            </span>
+                            <span class="icon">
+                                <a href="https://www.facebook.com/groups/yiitalk" target="_blank" rel="noopener">
+                                    <i class="fab fa-facebook-f fa-inverse" aria-hidden="true"></i>
+                                </a>
+                            </span>
+                            <span class="icon">
+                                <a href="https://twitter.com/yiiframework" target="_blank" rel="noopener">
+                                    <i class="fab fa-twitter fa-inverse" aria-hidden="true"></i>
+                                </a>
+                            </span>
+                            <span class="icon">
+                                <a href="https://t.me/yii3ru" target="_blank" rel="noopener">
+                                    <i class="fab fa-telegram-plane fa-inverse"></i>
+                                </a>
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </section>
-        </body>
-
-    <?php $this->endBody() ?>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
