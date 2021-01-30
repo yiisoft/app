3b3d04140657661b317c0da4195468233352558c
Alexander Makarov <sam@rmcreative.ru>
2021-01-30 03:40:08 +0300

    Add hint

--- config/common/router.php
+++ config/common/router.php
@@ -2,11 +2,14 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Config\Config;
 use Yiisoft\DataResponse\Middleware\FormatDataResponse;
 use Yiisoft\Router\Group;
 use Yiisoft\Router\RouteCollection;
 use Yiisoft\Router\RouteCollectionInterface;
 use Yiisoft\Router\RouteCollectorInterface;
+
+/** @var Config $config */
 
 return [
     RouteCollectionInterface::class => static function (RouteCollectorInterface $collector) use ($config) {
