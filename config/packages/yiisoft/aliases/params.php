78e9c4d109780fc3b26587bc1df1b1881b1f5ee8
Wilmer Arambula <terabytesoftw@gmail.com>
2021-04-08 10:54:04 -0400

    Remove config yii-web, move aliases to level app.

--- config/packages/yiisoft/aliases/params.php
+++ config/packages/yiisoft/aliases/params.php
@@ -4,13 +4,6 @@ declare(strict_types=1);
 
 return [
     'yiisoft/aliases' => [
-        'aliases' => [
-            '@root' => dirname(__DIR__, 4),
-            '@assets' => '@root/public/assets',
-            '@assetsUrl' => '/assets',
-            '@resources' => '@root/resources',
-            '@views' => '@root/resources/views',
-            '@message' => '@root/resources/message',
-        ],
+        'aliases' => [],
     ],
 ];
--- config/params.php
+++ config/params.php
@@ -8,4 +8,20 @@ return [
         'locale' => 'en',
         'name' => 'My Project',
     ],
+
+    'yiisoft/aliases' => [
+        'aliases' => [
+            '@root' => dirname(__DIR__),
+            '@assets' => '@root/public/assets',
+            '@assetsUrl' => '/assets',
+            '@baseUrl' => '/',
+            '@message' => '@root/resources/message',
+            '@npm' => '@root/node_modules',
+            '@public' => '@root/public',
+            '@resources' => '@root/resources',
+            '@runtime' => '@root/runtime',
+            '@vendor' => '@root/vendor',
+            '@views' => '@root/resources/views',
+        ],
+    ],
 ];
