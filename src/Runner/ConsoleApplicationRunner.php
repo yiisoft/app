17bdb743d7c7039f0f4b4836625e8ddde6cf2a78
Alexander Makarov <sam@rmcreative.ru>
2021-08-31 01:39:07 +0300

    Update namespace

--- src/Runner/ConsoleApplicationRunner.php
+++ src/Runner/ConsoleApplicationRunner.php
@@ -10,10 +10,10 @@ use Exception;
 use Psr\Container\ContainerInterface;
 use Yiisoft\Config\Config;
 use Yiisoft\Di\Container;
-use Yiisoft\Factory\Exception\CircularReferenceException;
-use Yiisoft\Factory\Exception\InvalidConfigException;
-use Yiisoft\Factory\Exception\NotFoundException;
-use Yiisoft\Factory\Exception\NotInstantiableException;
+use Yiisoft\Definitions\Exception\CircularReferenceException;
+use Yiisoft\Definitions\Exception\InvalidConfigException;
+use Yiisoft\Definitions\Exception\NotFoundException;
+use Yiisoft\Definitions\Exception\NotInstantiableException;
 use Yiisoft\Yii\Console\Application;
 use Yiisoft\Yii\Console\Output\ConsoleBufferedOutput;
 
