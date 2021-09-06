50ea13fddc21e8d51de63131a238f5606d663919
Alexander Makarov <sam@rmcreative.ru>
2021-09-06 13:52:25 +0300

    Update config/params-console-runner.php
    Co-authored-by: Sergei Predvoditelev <sergey.predvoditelev@gmail.com>

--- config/params-console-runner.php
+++ config/params-console-runner.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    // Environment mode for application console runner.
+    // Environment mode for console application runner.
     'env' => null,
     // Validate container config.
     'validateContainer' => true,
