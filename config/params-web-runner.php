7beefd7a18fcc74caec9873be2c26f680610198a
Alexander Makarov <sam@rmcreative.ru>
2021-09-06 13:52:45 +0300

    Update config/params-web-runner.php
    Co-authored-by: Sergei Predvoditelev <sergey.predvoditelev@gmail.com>

--- config/params-web-runner.php
+++ config/params-web-runner.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     // Debug mode for web application runner
     'debug' => true,
-    // Environment mode for application web runner.
+    // Environment mode for web application runner
     'env' => null,
     // Validate container config
     'validateContainer' => false,
