149501b9702846c005f561bb9cfc4299ed92b011
Sergei Predvoditelev <vjik@users.noreply.github.com>
2021-09-15 15:54:50 +0000

    Apply fixes from StyleCI

--- src/ViewInjection/LayoutViewInjection.php
+++ src/ViewInjection/LayoutViewInjection.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\ViewInjection;
 
-use App\ApplicationParameters;
 use Yiisoft\Assets\AssetManager;
 use Yiisoft\I18n\Locale;
 use Yiisoft\Router\UrlGeneratorInterface;
