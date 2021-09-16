30d21b6a521e2de8309a05e58e96d1055ae5701e
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-09-16 21:46:02 +0300

    Remove unnecessary code

--- resources/views/site/index.php
+++ resources/views/site/index.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 /** @var App\ApplicationParameters $applicationParameters */
 
-$this->params['breadcrumbs'] = '/';
-
 $this->setTitle($applicationParameters->getName());
 ?>
 
