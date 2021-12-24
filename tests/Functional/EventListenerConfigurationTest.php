f4396f08536e202625de32960b7ca9a14f194fe3
Rustam <rmamdaminov@gmail.com>
2021-12-24 17:43:58 +0500

    Fix tests

--- tests/Functional/EventListenerConfigurationTest.php
+++ tests/Functional/EventListenerConfigurationTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Functional;
 
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
+use Yiisoft\Config\ConfigPaths;
 use Yiisoft\Di\Container;
 use Yiisoft\Di\ContainerConfig;
 use Yiisoft\Yii\Event\ListenerConfigurationChecker;
