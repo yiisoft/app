60433608592cde4632b5bf3fb81ea442ffc56050
Dmitrii Derepko <xepozz@list.ru>
2022-11-05 21:13:53 +0300

    Use old class

--- config/common/translator.php
+++ config/common/translator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Translator\CategorySource;
+use Yiisoft\Translator\Formatter\Intl\IntlMessageFormatter;
 use Yiisoft\Translator\Message\Php\MessageSource;
 
 /** @var array $params */
@@ -14,7 +15,7 @@ return [
         return new CategorySource(
             $params['yiisoft/translator']['defaultCategory'],
             new MessageSource($aliases->get('@messages')),
-            new \Yiisoft\Translator\IntlMessageFormatter(),
+            new IntlMessageFormatter(),
         );
     },
 ];
