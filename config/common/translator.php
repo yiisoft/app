28760b0c78fc56675351b50c37888b5aa60c1b15
Dmitrii Derepko <xepozz@list.ru>
2022-11-05 21:02:03 +0300

    Fix translator config

--- config/common/translator.php
+++ config/common/translator.php
@@ -5,19 +5,16 @@ declare(strict_types=1);
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Translator\CategorySource;
 use Yiisoft\Translator\Message\Php\MessageSource;
-use Yiisoft\Translator\MessageFormatterInterface;
 
 /** @var array $params */
 
 return [
     // Configure application CategorySource
-    'translation.app' => static function (Aliases $aliases, MessageFormatterInterface $messageFormatter) use ($params) {
-        $messageSource = new MessageSource($aliases->get('@messages'));
-
+    'translation.app' => static function (Aliases $aliases) use ($params) {
         return new CategorySource(
             $params['yiisoft/translator']['defaultCategory'],
-            $messageSource,
-            $messageFormatter,
+            new MessageSource($aliases->get('@messages')),
+            new \Yiisoft\Translator\IntlMessageFormatter(),
         );
     },
 ];
