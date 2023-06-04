75cae5092b94a909cb7a1581683fbaf7c5d15222
Alexander Makarov <sam@rmcreative.ru>
2023-06-05 00:46:42 +0400

    Move urlGenerator to CommonViewInjection (#297)

--- src/ViewInjection/CommonViewInjection.php
+++ src/ViewInjection/CommonViewInjection.php
@@ -6,17 +6,21 @@ namespace App\ViewInjection;
 
 use App\ApplicationParameters;
 use Yiisoft\Yii\View\CommonParametersInjectionInterface;
+use Yiisoft\Router\UrlGeneratorInterface;
 
 final class CommonViewInjection implements CommonParametersInjectionInterface
 {
-    public function __construct(private ApplicationParameters $applicationParameters)
-    {
+    public function __construct(
+        private ApplicationParameters $applicationParameters,
+        private UrlGeneratorInterface $urlGenerator,
+    ) {
     }
 
     public function getCommonParameters(): array
     {
         return [
             'applicationParameters' => $this->applicationParameters,
+            'urlGenerator' => $this->urlGenerator,
         ];
     }
 }
