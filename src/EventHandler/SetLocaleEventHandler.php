aed8adf67f3c0d0bb03908905a5b2adef6458613
Tigrov <rrr-r@ya.ru>
2023-10-26 14:11:00 +0700

    Set current locale for WebView

--- src/EventHandler/SetLocaleEventHandler.php
+++ src/EventHandler/SetLocaleEventHandler.php
@@ -5,17 +5,20 @@ declare(strict_types=1);
 namespace App\EventHandler;
 
 use Yiisoft\Translator\TranslatorInterface;
+use Yiisoft\View\WebView;
 use Yiisoft\Yii\Middleware\Event\SetLocaleEvent;
 
 final class SetLocaleEventHandler
 {
     public function __construct(
-        private TranslatorInterface $translator
+        private TranslatorInterface $translator,
+        private WebView $webView
     ) {
     }
 
     public function handle(SetLocaleEvent $event): void
     {
         $this->translator->setLocale($event->getLocale());
+        $this->webView->setLocale($event->getLocale());
     }
 }
