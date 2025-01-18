5935bf645a39e59711372063f0c78360dd88efc4
Alexander Makarov <sam@rmcreative.ru>
2025-01-18 13:56:54 +0300

    Remove locale middleware

--- config/web/params.php
+++ config/web/params.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 use Yiisoft\ErrorHandler\Middleware\ErrorCatcher;
 use Yiisoft\Router\Middleware\Router;
 use Yiisoft\Session\SessionMiddleware;
-use Yiisoft\Yii\Middleware\Locale;
 
 return [
     'middlewares' => [
         ErrorCatcher::class,
         SessionMiddleware::class,
-        Locale::class,
         Router::class,
     ],
 
