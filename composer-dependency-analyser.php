0568953bcad9a5a208ec86bfe50fc6e7aa7b687b
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-07-15 17:24:27 +0300

    Replace composer require checker to composer dependency analyzer (#362)

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+$root = __DIR__;
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan($root . '/src', isDev: false)
+    ->addPathToScan($root . '/config', isDev: false)
+    ->addPathToScan($root . '/resources/views', isDev: false)
+    ->addPathToScan($root . '/public/index.php', isDev: false)
+    ->addPathToScan($root . '/yii', isDev: false)
+    ->addPathToScan($root . '/tests', isDev: true)
+    ->ignoreErrorsOnPackage('psr/container', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV])
+    ->ignoreErrorsOnPackage('yiisoft/config', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV])
+    ->ignoreErrorsOnPackage('yiisoft/yii-event', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV])
+    ->ignoreErrorsOnPackage('yiisoft/router-fastroute', [ErrorType::UNUSED_DEPENDENCY]);
