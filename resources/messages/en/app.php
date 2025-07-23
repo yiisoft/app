653d12796af1db7c08a4cd32006cab82067c0357
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-07-23 14:17:00 +0300

    Remove translator (#367)

--- resources/messages/en/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'site.hello' => 'Hello',
-    'site.start_with' => 'Let\'s start something great with <strong>Yii3</strong>',
-    'site.guide_remind' => 'Don\'t forget to check the guide',
-];
--- src/Controller/HomePage/template.php
+++ src/Controller/HomePage/template.php
@@ -3,12 +3,10 @@
 declare(strict_types=1);
 
 use App\ApplicationParameters;
-use Yiisoft\Translator\TranslatorInterface;
 use Yiisoft\View\WebView;
 
 /**
  * @var WebView $this
- * @var TranslatorInterface $translator
  * @var ApplicationParameters $applicationParameters
  */
 
@@ -16,13 +14,13 @@ $this->setTitle($applicationParameters->name);
 ?>
 
 <div class="text-center">
-    <h1><?= $translator->translate('site.hello')?>!</h1>
+    <h1>Hello!</h1>
 
-    <p><?= $translator->translate('site.start_with')?>!</p>
+    <p>Let's start something great with <strong>Yii3</strong>!</p>
 
     <p>
         <a href="https://github.com/yiisoft/docs/tree/master/guide/en" target="_blank" rel="noopener">
-            <i><?= $translator->translate('site.guide_remind')?>.</i>
+            <i>Don't forget to check the guide.</i>
         </a>
     </p>
 </div>
