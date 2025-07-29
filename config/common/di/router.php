920227e636422953d3833239fcc8252a9e5e1f46
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-07-29 21:44:53 +0300

    Refactor configuration (#371)

--- config/common/di/router.php
+++ config/common/di/router.php
@@ -3,21 +3,20 @@
 declare(strict_types=1);
 
 use Yiisoft\Config\Config;
-use Yiisoft\Csrf\CsrfTokenMiddleware;
-use Yiisoft\DataResponse\Middleware\FormatDataResponse;
+use Yiisoft\Definitions\DynamicReference;
 use Yiisoft\Router\RouteCollection;
 use Yiisoft\Router\RouteCollectionInterface;
-use Yiisoft\Router\RouteCollectorInterface;
+use Yiisoft\Router\RouteCollector;
 
 /** @var Config $config */
 
 return [
-    RouteCollectionInterface::class => static function (RouteCollectorInterface $collector) use ($config) {
-        $collector
-            ->middleware(CsrfTokenMiddleware::class)
-            ->middleware(FormatDataResponse::class)
-            ->addRoute(...$config->get('routes'));
-
-        return new RouteCollection($collector);
-    },
+    RouteCollectionInterface::class => [
+        'class' => RouteCollection::class,
+        '__construct()' => [
+            'collector' => DynamicReference::to(
+                static fn() => (new RouteCollector())->addRoute(...$config->get('routes')),
+            ),
+        ],
+    ],
 ];
--- config/web/params.php
+++ config/web/params.php
@@ -2,14 +2,4 @@
 
 declare(strict_types=1);
 
-use Yiisoft\ErrorHandler\Middleware\ErrorCatcher;
-use Yiisoft\Router\Middleware\Router;
-use Yiisoft\Session\SessionMiddleware;
-
-return [
-    'middlewares' => [
-        ErrorCatcher::class,
-        SessionMiddleware::class,
-        Router::class,
-    ],
-];
+return [];
