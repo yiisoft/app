e5bcb94e621d564b0ea660db30abfc3a546548a1
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-19 10:34:42 +0300

    Update rector configuration

--- rector.php
+++ rector.php
@@ -6,24 +6,17 @@ use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withPhpSets(php82: true)
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ])
+    ->withSkip([
         ClosureToArrowFunctionRector::class,
         ReadOnlyPropertyRector::class,
     ]);
-};
