f47aec95236c50517e5aabef080d15ec300552f1
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-20 13:26:25 +0300

    Simplify `MainAsset`

--- src/Layout/Main/MainAsset.php
+++ src/Layout/Main/MainAsset.php
@@ -5,20 +5,13 @@ declare(strict_types=1);
 namespace App\Layout\Main;
 
 use Yiisoft\Assets\AssetBundle;
-use Yiisoft\Assets\AssetManager;
 
-/**
- * @psalm-import-type CssFile from AssetManager
- */
 final class MainAsset extends AssetBundle
 {
     public ?string $basePath = '@assets/main';
     public ?string $baseUrl = '@assetsUrl/main';
     public ?string $sourcePath = '@assetsSource/main';
 
-    /**
-     * @psalm-var array<array-key, CssFile|string>
-     */
     public array $css = [
         'site.css',
     ];
